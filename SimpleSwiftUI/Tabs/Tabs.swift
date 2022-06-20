5192808df8cc511e5103e4b44a392a57f29a2a1a
jblackburn <john@digitalglen.com>
2022-06-19 19:12:05 -0700

    + SceneStorage: currently selected tab

--- SimpleSwiftUI/Tabs/Tabs.swift
+++ SimpleSwiftUI/Tabs/Tabs.swift
@@ -1,48 +1,49 @@
 import SwiftUI
 
-enum Tab {
-    case dev
-    case today
-    case store
-    case library
-    case settings
+enum Tab: Int {
+    case today = 0
+    case store = 1
+    case library = 2
+    case settings = 3
+    case dev = 4
 }
 
 struct Tabs: View {
-    @State private var tabSelection: Tab = .library
+    @SceneStorage("selectedTab")
+    private var selectedTab: Int = Tab.today.rawValue
     
     var body: some View {
-        TabView(selection: $tabSelection) {
+        TabView(selection: $selectedTab) {
             Tab_Today()
                 .tabItem {
                     Label("Today", systemImage: "sun.max")
                     Text("Today")
                 }
-                .tag(Tab.today)
+                .tag(Tab.today.rawValue)
             Tab_Store()
                 .tabItem {
                     Label("Store", systemImage: "square.grid.2x2")
                     Text("Store")
                 }
-                .tag(Tab.store)
+                .tag(Tab.store.rawValue)
             Tab_Library()
                 .tabItem {
                     Label("Library", systemImage: "person")
                     Text("Library")
                 }
-                .tag(Tab.library)
+                .tag(Tab.library.rawValue)
             Tab_Settings()
                 .tabItem {
                     Label("Settings", systemImage: "slider.horizontal.3")
                     Text("Settings")
                 }
-                .tag(Tab.settings)
+                .tag(Tab.settings.rawValue)
             Tab_Dev()
                 .tabItem {
                     Label("Dev", systemImage: "wrench")
                     Text("Dev")
                 }
-                .tag(Tab.dev)
+                .tag(Tab.dev.rawValue)
         }
         .accentColor( Color( UIColor.label ) )
         .preferredColorScheme(.dark)
